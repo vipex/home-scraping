426aa6f1fa0e715b7f4ec36d012b3831c713844d
vipex <luca.ravizzotti@gmail.com>
2016-08-24 11:52:41 +0200

    Casa spider almost finished

--- hs/spiders/casa.py
+++ hs/spiders/casa.py
@@ -16,6 +16,14 @@ BASEDOMAIN = "http://www.casa.it"
 BASEQUERY = "/vendita-residenziale/in-parella++torino%2c+to%2c+piemonte%3b+pozzo+strada++torino%2c+to%2c+piemonte%3b+cenisia++torino%2c+to%2c+piemonte%3b+cit+turin++torino%2c+to%2c+piemonte%3b+san+donato++torino%2c+to%2c+piemonte%3b+centro++giardini+reali++repubblica++torino%2c+to%2c+piemonte%3b+crocetta++torino%2c+to%2c+piemonte%3b+san+paolo++torino%2c+to%2c+piemonte%3b+rivoli++to%2c+piemonte%3b+grugliasco++to%2c+piemonte%3b+collegno++to%2c+piemonte/lista-1"
 
 
+def find_feature(feats, name):
+    for f in feats:
+        if f['label'].lower() == name.lower():
+            return f['value']
+
+    return ''
+
+
 class CasaSpider(scrapy.Spider):
     name = "casa"
     allowed_domains = ["casa.it"]
@@ -75,23 +83,21 @@ class CasaSpider(scrapy.Spider):
 
         item['price'] = jsonobj['detail']['_map']['price']
 
-        item['listingRef'] = jsonobj['detail']['_map']['']
-        item['listingDate'] = jsonobj['detail']['_map']['']
+        item['listingRef'] = jsonobj['detail']['_map']['rif']
+        item['listingDate'] = jsonobj['detail']['_map']['modifiedDate']
         item['contract'] = jsonobj['detail']['_map']['channel']
         item['propertyType'] = jsonobj['detail']['_map']['type']
-        item['area'] = jsonobj['detail']['_map']['']
-        item['rooms'] = jsonobj['detail']['_map']['']
-        item['floor'] = jsonobj['detail']['_map']['']
-        item['box'] = jsonobj['detail']['_map']['']
-        item['availability'] = jsonobj['detail']['_map']['']
-        item['year'] = jsonobj['detail']['_map']['']
-        item['condition'] = jsonobj['detail']['_map']['']
-        item['heating'] = jsonobj['detail']['_map']['']
-        item['energyEP'] = jsonobj['detail']['_map']['']
-        item['energyClass'] = jsonobj['detail']['_map']['']
-        item['address'] = jsonobj['detail']['_map']['']
-        item['address'] = jsonobj['detail']['_map']['']
-        item['agency'] = jsonobj['detail']['_map']['']
-        # TODO: Create parser
+        item['area'] = jsonobj['detail']['_map']['landsize']
+        item['rooms'] = jsonobj['detail']['_map']['bedrooms']
+        item['floor'] = find_feature(jsonobj['detail']['_map']['features'], 'Piano')
+        item['box'] = jsonobj['detail']['_map']['parkingspaces']
+        item['availability'] = find_feature(jsonobj['detail']['_map']['features'], 'Stato al rogito')
+        item['year'] = find_feature(jsonobj['detail']['_map']['features'], 'Anno di costruzione')
+        item['condition'] = find_feature(jsonobj['detail']['_map']['features'], 'Condizioni')
+        item['heating'] = find_feature(jsonobj['detail']['_map']['features'], 'Riscaldamento')
+        item['energyEP'] = jsonobj['detail']['_map']['energyClass']['ipe']
+        item['energyClass'] = jsonobj['detail']['_map']['energyClass']['class']
+        item['address'] = jsonobj['detail']['_map']['address']
+        item['agency'] = jsonobj['detail']['_map']['agency']
 
         yield item
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Main runner
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+
+from hs.spiders.casa import CasaSpider
 from hs.spiders.immobiliare import ImmobiliareSpider
 
 # Use runner object to run the spider(s)
 runner = CrawlerProcess(get_project_settings())
-runner.crawl(ImmobiliareSpider)
-# runner.crawl(CasaSpider)
+# runner.crawl(ImmobiliareSpider)
+runner.crawl(CasaSpider)
 runner.start()
